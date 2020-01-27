408a11cf6c083bcb76257a837d67a5e0677fb013
Lucca Radaeli <radaelidev@gmail.com>
2020-01-27 18:23:03 +0000

    fixes

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {getToken} from './auth'
 
 const api = axios.create({
-  baseURL: 'https://findevs-backend.herokuapp.com/',
+  baseURL: 'https://3333-b6f71042-d042-4432-8322-2275dd59af62.ws-us02.gitpod.io',
 });
 
 api.interceptors.request.use(async config => {
